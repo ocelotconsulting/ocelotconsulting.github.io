c587ee15cbbbbab8d823b825514d1a4536bcf9c5
Drew Garrett <drew.garrett@ocelotconsulting.com>
2023-04-25 11:55:00 -0500

    Fixing home swiper left for #141

--- src/components/SwiperPrev.tsx
+++ src/components/SwiperPrev.tsx
@@ -4,7 +4,7 @@ import {faChevronLeft} from '@fortawesome/free-solid-svg-icons/faChevronLeft'
 
 export default function SwiperPrev() {
     return (
-        <button className="button-next absolute top-1/2 -left-5 z-10 -translate-y-1/2 w-10 h-10 bg-black text-white flex flex-col justify-center items-center">
+        <button className="button-prev absolute top-1/2 -left-5 z-10 -translate-y-1/2 w-10 h-10 bg-black text-white flex flex-col justify-center items-center">
             <FontAwesomeIcon icon={faChevronLeft} />
         </button>
     )
