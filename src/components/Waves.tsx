7bd09181c353914deb629a6b48c4c6bcfb813bfa
Drew Garrett <drew.garrett@ocelotconsulting.com>
2023-04-27 10:10:59 -0500

    Persist pause state in localStorage

--- src/components/Waves.tsx
+++ src/components/Waves.tsx
@@ -1,21 +1,59 @@
 // @ts-ignore
-import Lottie from 'react-lottie'
-import {ComponentPropsWithoutRef} from 'react'
+import { Player, Controls, PlayerState } from '@lottiefiles/react-lottie-player'
+import { ComponentPropsWithoutRef, createRef, useState, useEffect } from 'react'
 import animationData from '@/public/waves.json'
 
-const waveOptions = {
-    loop: true,
-    autoplay: true,
-    animationData,
-    rendererSettings: {
+export default function Waves(props: ComponentPropsWithoutRef<'div'>) {
+    const localStorageKey = "wavesAutoplay"
+
+    // Default is true
+    const wavesAutoplayDefault = true
+
+    // Load history from localStorage
+    const [wavesAutoplay, setAutoplay] = useState(() => {
+        if (typeof window === 'undefined') return wavesAutoplayDefault
+
+        const stored = localStorage.getItem(localStorageKey);
+
+        if (stored) {
+            const parsed = JSON.parse(stored)
+
+            if (typeof parsed === 'boolean') {
+                return parsed
+            }
+        }
+
+        return wavesAutoplayDefault
+    })
+
+    // Updating localStorage on change
+    useEffect(() => {
+        localStorage.setItem(localStorageKey, JSON.stringify(wavesAutoplay));
+    }, [wavesAutoplay]);
+
+    const rendererSettings = {
         preserveAspectRatio: "xMidYMid slice"
     }
-}
 
-export default function Waves(props: ComponentPropsWithoutRef<'div'>) {
+    const player = createRef<Player>()
+
     return (
-        <div {...props}>
-            <Lottie options={waveOptions} />
+        <div {...props} onClick={() => player.current?.state.playerState === PlayerState.Playing ? player.current?.pause() : player.current?.play() }>
+            <Player
+                ref={player}
+                loop={true}
+                autoplay={wavesAutoplay}
+                controls={true}
+                src={JSON.stringify(animationData)}
+                rendererSettings={rendererSettings}
+
+                onEvent={event => {
+                    // Save our pause state
+                    if (event === 'pause') setAutoplay(false);
+                    if (event === 'play') setAutoplay(true);
+                }}
+            >
+            </Player>
         </div>
     )
 }
