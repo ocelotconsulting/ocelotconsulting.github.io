3fc7ea4ae08b9e0986ec9620e7eb79803e5112dd
Drew <nerddtvg@users.noreply.github.com>
2024-01-26 12:50:53 -0600

    Adding Privacy and Cookie Policies (#214)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -67,19 +67,6 @@ export default function App({ Component, pageProps }: AppProps) {
                 <meta property="og:image:type" content="image/svg+xml" />
                 <meta name="twitter:image" content={`${origin}/assets/ocelot.svg`} />
             </Head>
-            <Script
-                src="https://www.googletagmanager.com/gtag/js?id=UA-84294052-1"
-                strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'UA-84294052-1');
-                `}
-            </Script>
             <Header setShowMenu={setShowMenu} setShowContact={setShowContact} />
             <main>
                 <Component {...pageProps} setShowContact={setShowContact} />
