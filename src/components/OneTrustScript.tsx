eb0d69524b903fb1748c20935039821fb6feb3e2
Drew Garrett <drew.garrett@ocelotconsulting.com>
2024-02-08 11:41:51 -0600

    Added NEXT_PUBLIC_ONE_TRUST_DOMAIN_SCRIPT variable

--- src/components/OneTrustScript.tsx
+++ src/components/OneTrustScript.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script'
 export default function OneTrustScript() {
     return (
         <>
-            <Script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" strategy='beforeInteractive' type="text/javascript" charSet="UTF-8" data-domain-script="3a6bb61d-8924-4c57-a2b6-bd2306a9c5ef-test"></Script>
+            <Script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" strategy='beforeInteractive' type="text/javascript" charSet="UTF-8" data-domain-script={process.env.NEXT_PUBLIC_ONE_TRUST_DOMAIN_SCRIPT ?? ''}></Script>
             <Script type="text/javascript" strategy='beforeInteractive'>
                 {`function OptanonWrapper() { }`}
             </Script>
